f0039cb62c1f63cbc7fb4b215d0cc1d90d8f6191
selmaaksoy <selmaaksoy2004@gmail.com>
2023-04-03 13:06:33 +0200

    Initial Commit

---
+++ calculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  calculator
+//
+//  Created by Selma  Aksoy on 3.4.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
